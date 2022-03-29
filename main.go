594f080ea22e934647ab6d5ad11d81c77d160a56
nexryai <nexryai@outlook.com>
2022-03-29 13:52:10 +0900

    コマンドの実行関数の追加

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"os/exec"
 	"flag"
 	"fmt"
 	"strings"
@@ -16,7 +17,26 @@ func msgErr(err string){
 	fmt.Printf("\x1b[31m%s\n", "===ERROR===")
 	fmt.Fprintln(os.Stderr, err)
 	fmt.Printf("\x1b[0m%s\n", "\n")
+}
 
+func execCmd(cmd string, arg string){
+	fmt.Printf("\x1b[34m%s\n", "[EXEC]")
+	fmt.Println("Command:", cmd, arg)
+
+	out, err := exec.Command(cmd, arg).Output()
+
+
+	if err != nil {
+		msg := "Failed to execute " + cmd + "\n 	>>> " + err.Error()
+		msgErr(msg)
+	} else {
+		fmt.Println(" ⌵\n ⌵\n ⌵")
+		msg := "===Stdout===\n" + (string(out))
+		msgInfo(msg)
+	}
+
+
+	fmt.Printf("\x1b[0m%s\n", "\n")
 }
 
 func main() {
@@ -41,8 +61,14 @@ func main() {
 	var distVersion = arr[1]
 
 	fmt.Println("Distribution:", distName, " Version:", distVersion, " User:", username)
+
+	if (distName == "debian") {
+		buildDebian(distVersion, username)
+	}
+
 }
 
-func buildDebian(dist string, username string) {
-	fmt.Println("Build debian container...")
+func buildDebian(debianVer string, username string) {
+	msgInfo("Build debian container...")
+	execCmd("zypper", "refresh")
 }
